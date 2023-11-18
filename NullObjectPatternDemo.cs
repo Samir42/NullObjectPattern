9909cd03bc641820595fc409e52797ea06a88206
Samir Osmanov <45008446+Samir42@users.noreply.github.com>
2023-11-18 16:22:27 +0400

    Update NullObjectPatternDemo.cs

--- NullObjectPatternDemo.cs
+++ NullObjectPatternDemo.cs
@@ -1,1 +1,44 @@
+public interface IUser 
+{
+  public string FirstName { get; }
+  public string LastName { get; }
+}
 
+public class User: IUser 
+{
+  public string FirstName { get; private set; }
+  public string LastName { get; private set; }
+
+  public User(string firstName, string lastName)
+  {
+    FirstName = firstName;
+    LastName = lastName;
+  }
+}
+
+public class NullUser: IUser 
+{
+  public string FirstName { get; } = string.Empty;
+  public string LastName { get; } = string.Empty;
+}
+
+
+public void SaveUser(User userToSave)
+{
+  if (userToSave == null) throw new ArgumentNullException(nameof(userToSave));
+  if (userToSave.FirstName == null) throw new ArgumentNullException(nameof(userToSave.FirstName));
+  if (userToSave.LastName == null) throw new ArgumentNullException(nameof(userToSave.LastName));
+
+  // Save user...
+}
+
+
+public User GetUserById(int userId)
+{
+  var userFromDb = _userRepo.GetUserById(userId);
+
+  if (userFromDb is null)
+    return new NullUser();
+
+  return userFromDb;
+}
